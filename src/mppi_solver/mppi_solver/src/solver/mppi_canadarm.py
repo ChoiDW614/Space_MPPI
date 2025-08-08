3fad244d67c4902b674723f7677fe7ffe36fd92f
cold-deuu <lee081847@gmail.com>
2025-08-08 15:29:45 +0900

    chan_commit

--- src/mppi_solver/mppi_solver/src/solver/mppi_canadarm.py
+++ src/mppi_solver/mppi_solver/src/solver/mppi_canadarm.py
@@ -37,6 +37,8 @@ from pathlib import Path
 from mppi_solver.src.utils.matlab_logger import MATLABLogger
 from mppi_solver.src.utils.time import Time
 
+# Chan 250804
+from mppi_solver.src.solver_v2.sampling.standard_normal_noise import StandardSamplling
 
 class MPPI():
     def __init__(self, params):
@@ -81,7 +83,7 @@ class MPPI():
         self.u_prev = torch.zeros((self.n_horizon, self.n_action), **self.tensor_args)
 
         # Sampling class
-        self.sample_gen = MixedSampling(self.params, self.tensor_args)
+        # self.sample_gen = MixedSampling(self.params, self.tensor_args)
 
         # base control states
         self.base_pose = Pose(self.tensor_args)
@@ -105,6 +107,15 @@ class MPPI():
         urdf_file_path = os.path.join(get_package_share_directory(package_name), "models", "canadarm", urdf_name)
 
         # Forward kinematics
+        # if self.is_compile:
+        #     try:
+        #         fk_canadarm = URDFForwardKinematics(params=self.params, urdf=urdf_file_path, root_link='Base_SSRMS', end_links='EE_SSRMS_tip', tensor_args=self.tensor_args)
+        #         self.fk_canadarm = torch.compile(fk_canadarm, fullgraph=True, mode="reduce-overhead")
+        #     except Exception as e:
+        #         self.logger.warning(f"Failed to compile URDF Kinematics: {e}")
+        #         self.logger.info(f"Using non-compiled URDF Kinematics")
+        #         self.fk_canadarm = URDFForwardKinematics(params=self.params, urdf=urdf_file_path, root_link='Base_SSRMS', end_links='EE_SSRMS_tip', tensor_args=self.tensor_args)
+        # else:
         self.fk_canadarm = URDFForwardKinematics(params=self.params, urdf=urdf_file_path, root_link='Base_SSRMS', end_links='EE_SSRMS_tip', tensor_args=self.tensor_args)
 
         # Filter
@@ -123,6 +134,15 @@ class MPPI():
         self.param_gamma = self._lambda * (1.0 - 0.9)
         self.is_reaching = False
 
+        # if self.is_compile:
+        #     try:
+        #         calc_jacob = CanadarmJacob(params, self.tensor_args)
+        #         self.calc_jacob = torch.compile(calc_jacob, fullgraph=True)
+        #     except Exception as e:
+        #         self.logger.warning(f"Failed to compile CanadarmJacob: {e}")
+        #         self.logger.info(f"Using non-compiled CanadarmJacob")
+        #         self.calc_jacob = CanadarmJacob(params, self.tensor_args)
+        # else:
         self.calc_jacob = CanadarmJacob(params, self.tensor_args)
 
         # Log
@@ -131,10 +151,9 @@ class MPPI():
         self.matlab_logger.create_dataset(dataset_name="end_effector_pose", shape=7)
         self.matlab_logger.create_dataset(dataset_name="pos_err", shape=4)
         self.matlab_logger.create_dataset(dataset_name="ori_err", shape=4)
-        self.matlab_logger.create_dataset(dataset_name="cost", shape=10)
+        self.matlab_logger.create_dataset(dataset_name="cost", shape=9)
         self.matlab_logger.create_dataset(dataset_name="sigma", shape=(self.n_action+1))
         self.matlab_logger.create_dataset(dataset_name="base", shape=7)
-        self.matlab_logger.create_dataset(dataset_name="torque", shape=8)
 
         # test
         # from torch.utils.tensorboard import SummaryWriter
@@ -146,10 +165,8 @@ class MPPI():
         self.reference_se3 = None
         self.iteration = 0
 
-        # Torque
-        self.state_M = None
-        self.state_G = None
-        self.torque_sum = torch.zeros((self.n_action), **self.tensor_args)
+        # Chan
+        self.sampling = StandardSamplling(self.n_samples, self.n_horizon, self.n_action, self.device)
         
 
     def check_reach(self):
@@ -167,10 +184,18 @@ class MPPI():
         # diff_ori_quat = matrix_to_quaternion(diff_ori_mat)
         self.diff_ori_3d = matrix_to_euler_angles(diff_ori_mat, "ZYX")
         self.target_dist = torch.norm(self.ee_pose.pose - self.target_pose.pose, p=2)
+        
+        # self.logger.info(f"EE : {ee_pose_mat}")
+        # self.logger.info(f"Target : {self.target_dist}")
+        # self.logger.info(f"target dist: {self.target_dist}")
+        # self.logger.info(f"target pose: {target_ori_mat}")
+        # self.logger.info(f"EE POSE : {self.ee_pose.pose[0]:.2f}, {self.ee_pose.pose[1]:.2f}, {self.ee_pose.pose[2]:.2f}, {self.ee_pose.orientation[0]:.2f}, {self.ee_pose.orientation[1]:.2f}, {self.ee_pose.orientation[2]:.2f}, {self.ee_pose.orientation[3]:.2f}")
 
-        self.logger.info(f"target dist: {self.target_dist}")
+        # Chan
+        self.cost_manager.update_weights(self.target_dist)
 
-        if self.target_dist < 0.05:
+
+        if self.target_dist < 0.01:
             return True
         else:
             return False
@@ -178,53 +203,52 @@ class MPPI():
 
     def compute_control_input(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         if self.check_reach():
-            self.logger.info(f"reach time: {self.sim_time.time}")
             return self.qdes, self.vdes, self.u
         
-        self.MATLAB_log()
+        # self.MATLAB_log()
 
         u = self.u_prev.clone()
-        noise = self.sample_gen.sampling()
+        # noise = self.sample_gen.sampling()
+        noise = self.sampling.sampling()
         v = u + noise
 
-        qSamples, vSamples = self.sample_gen.get_sample_joint(v, self._q, self._qdot, self.dt)
+        qSamples, vSamples = self.sampling.get_sample_joint(v, self._q, self._qdot, self.dt)
         qSamples = qSamples.to(**self.tensor_args)
         vSamples = vSamples.to(**self.tensor_args)
 
         trajectory, link_list, com_list = self.fk_canadarm(qSamples,
                                         'EE_SSRMS_tip', 'Base_SSRMS', self.base_pose.tf_matrix(self.tensor_args))
 
-        jacob    = self.calc_jacob(com_list, link_list, bm=False)
-        jacob_bm, H_star = self.calc_jacob(com_list, link_list, jacob, bm=True)
-        torque   = torch.einsum('ij,btj->bti', self.state_M, v) + self.state_G
-
+        jacob = self.calc_jacob(com_list, link_list, bm=False)
+        jacob_bm = self.calc_jacob(com_list, link_list, jacob, bm=True)
+        # self.logger.info(f"Traj : {trajectory}")
         self.cost_manager.update_pose_cost(qSamples, v, vSamples, trajectory, self.reference_joint, self.reference_se3, self.target_pose)
-        self.cost_manager.update_covar_cost(u, v, self.sample_gen.sigma_matrix)
-        self.cost_manager.update_base_cost(self.base_pose, self._q)
-        self.cost_manager.update_collision_cost(self.collision_target)
-        self.cost_manager.update_ee_cost(jacob, self.target_dist)
-        self.cost_manager.update_reference_cost(link_list[...,-2])
+        self.cost_manager.update_covar_cost(u, v, self.sampling.sigma_matrix)
+        # self.cost_manager.update_base_cost(self.base_pose, self._q)
+        # self.cost_manager.update_collision_cost(self.collision_target)
+        # self.cost_manager.update_ee_cost(jacob, self.target_dist)
+        # self.cost_manager.update_reference_cost(link_list[...,-2])
         self.cost_manager.update_base_disturbance_cost(jacob_bm)
-        self.cost_manager.update_energy_cost(torque, H_star)
 
         S = self.cost_manager.compute_all_cost()
 
         w = self.compute_weights(S, self._lambda)
         w_expanded = w.view(-1, 1, 1)
         w_eps = torch.sum(w_expanded * noise, dim = 0)
+        # w_eps = self.svg_filter.savgol_filter_torch(w_eps, window_size=9, polyorder=2, tensor_args=self.tensor_args)
 
         u += w_eps
 
-        self.sample_gen.update_distribution(u, v, w, noise)
+        # self.sample_gen.update_distribution(u, v, w, noise)
 
         self.u_prev = u.clone()
         self.u = u[0].clone()
         self.noise_prev = noise.clone()
 
         self.vdes = self._qdot + self.u * self.dt
-        self.qdes = self._q + self._qdot * self.dt + 0.5 * self.u * self.dt * self.dt
+        self.qdes = self._q + self._qddot * self.dt + 0.5 * self.u * self.dt * self.dt
         return self.qdes, self.vdes, self.u
-    
+
 
     def compute_weights(self, S: torch.Tensor, _lambda: float) -> torch.Tensor:
         weights = torch.softmax(-S / _lambda, dim=0)  # (n_samples,)
@@ -232,7 +256,9 @@ class MPPI():
     
 
     def MATLAB_log(self):
-        q_prev, self.v_prev = self.sample_gen.get_prev_sample_joint(self.u_prev, self._q, self._qdot, self.dt)
+        # q_prev, self.v_prev = self.sample_gen.get_prev_sample_joint(self.u_prev, self._q, self._qdot, self.dt)
+        q_prev, self.v_prev = self.sampling.get_prev_sample_joint(self.u_prev, self._q, self._qdot, self.dt)
+
         self.fk_canadarm.set_samples_and_timesteps(n_samples=1, n_horizon=self.n_horizon)
         ee_traj_prev, link_list_prev, com_list_prev = self.fk_canadarm(q_prev,
                                                     'EE_SSRMS_tip', 'Base_SSRMS', self.base_pose.tf_matrix(self.tensor_args))
@@ -240,12 +266,8 @@ class MPPI():
         
         ee_traj_prev = ee_traj_prev.squeeze(0).cpu()
         ee_jacobian_prev = self.calc_jacob(com_list_prev, link_list_prev, bm=False)
-        jacob_bm, H_star = self.calc_jacob(com_list_prev, link_list_prev, ee_jacobian_prev, bm=True)
+        jacob_bm = self.calc_jacob(com_list_prev, link_list_prev, ee_jacobian_prev, bm=True)
         ee_jacobian_prev = ee_jacobian_prev.squeeze(0)
-        torque_prev = self.state_M @ self.u_prev[0] + self.state_G
-        # self.torque_sum = self.torque_sum + torque_prev
-
-        # self.logger.info(f"H_star: {is_psd(H_star[0,:,:,:])}")
 
         prev_stage_cost     = self.cost_manager.pose_cost.compute_prev_stage_cost(ee_traj_prev, self.target_pose)
         prev_terminal_cost  = self.cost_manager.pose_cost.compute_prev_terminal_cost(ee_traj_prev, self.target_pose)
@@ -258,7 +280,6 @@ class MPPI():
         prev_ee_cost        = self.cost_manager.ee_cost.compute_prev_ee_cost(self.v_prev, ee_jacobian_prev, self.target_dist)
         prev_reference_cost = self.cost_manager.reference_cost.compute_prev_reference_cost(link_list_prev[...,-2], self.reference_se3)
         prev_disturbance_cost = self.cost_manager.disturbace_cost.compute_prev_base_disturbance_cost(jacob_bm, self.v_prev)
-        prev_energy_cost    = self.cost_manager.energy_cost.compute_prev_energy_cost(torque_prev, self.v_prev, H_star)
         
         mean_prev_stage_cost     = torch.mean(prev_stage_cost)
         mean_prev_terminal_cost  = torch.mean(prev_terminal_cost)
@@ -271,25 +292,25 @@ class MPPI():
         mean_prev_ee_cost        = torch.mean(prev_ee_cost)
         mean_prev_reference_cost = torch.mean(prev_reference_cost)
         mean_prev_disturbance_cost = torch.mean(prev_disturbance_cost)
-        mean_prev_energy_cost    = torch.mean(prev_energy_cost)
         
         self.matlab_logger.log("end_effector_pose", [self.sim_time.time] + self.ee_pose.np_pose.tolist() + self.ee_pose.np_rpy.tolist())
         self.matlab_logger.log("pos_err", [self.sim_time.time] + (self.ee_pose.np_pose - self.target_pose.np_pose).tolist())
         self.matlab_logger.log("ori_err", [self.sim_time.time] + self.diff_ori_3d.tolist())
         self.matlab_logger.log("cost", [self.sim_time.time] + [mean_prev_stage_cost.item(),
                                                                mean_prev_terminal_cost.item(),
+                                                            #    mean_prev_covar_cost.item(),
+                                                            #    mean_prev_centering_cost.item(),
+                                                            #    mean_prev_tracking_cost.item(),
                                                                mean_prev_action_cost.item(),
                                                                mean_prev_collision_cost.item(),
                                                                mean_prev_stop_cost.item(),
                                                                mean_prev_ee_cost.item(),
                                                                mean_prev_reference_cost.item(),
-                                                               mean_prev_disturbance_cost.item(),
-                                                               mean_prev_energy_cost.item()])
-        self.matlab_logger.log("sigma", [self.sim_time.time] + torch.diag(self.sample_gen.sigma).tolist())
+                                                               mean_prev_disturbance_cost.item()])
+        self.matlab_logger.log("sigma", [self.sim_time.time] + torch.diag(self.sampling.sigma).tolist())
+        # self.matlab_logger.log("sigma", [self.sim_time.time] + torch.diag(self.sample_gen.sigma).tolist())
         self.matlab_logger.log("base", [self.sim_time.time] + \
                                self.cost_manager.disturbace_cost.compute_base_disturbance(jacob_bm, self.v_prev).tolist())
-        self.matlab_logger.log("torque", [self.sim_time.time] + torque_prev.tolist())
-        
         return
     
 
@@ -304,12 +325,15 @@ class MPPI():
     def set_target_pose(self, pose: Pose):
         self.target_pose.pose = pose.pose.to(**self.tensor_args)
         self.target_pose.orientation = pose.orientation.to(**self.tensor_args)
+
+        self.logger.info(f"Target Ori : {self.target_pose.orientation}")
         return
     
     def set_base_pose(self, pos, ori):
         self.base_pose.pose = pos
         self.base_pose.orientation = ori
         self.calc_jacob.base_update(self.base_pose.tf_matrix())
+        self.logger.info(f"Base Pose Mat : {self.base_pose.tf_matrix()}")
         return
     
     def set_collision_target(self, target: torch.Tensor):
@@ -320,10 +344,6 @@ class MPPI():
         self.reference_joint = reference_joint.clone()
         self.reference_se3 = reference_se3.clone()
         return
-    
-    def setstate_mass_nle(self, M: np.ndarray, G: np.ndarray):
-        self.state_M = torch.from_numpy(M).to(**self.tensor_args)
-        self.state_G = torch.from_numpy(G).to(**self.tensor_args)
 
     def warm_up(self):
         qSamples = torch.zeros((1, self.n_horizon, self.n_action), **self.tensor_args)
@@ -337,29 +357,3 @@ class MPPI():
         jaco = self.calc_jacob(com_list, link_list, bm=False)
         self.calc_jacob(com_list, link_list, jaco, bm=True)
         return
-
-
-def is_psd(A: torch.Tensor, tol: float = 1e-8) -> torch.BoolTensor:
-    """
-    Check if each matrix A[..., i, j] is symmetric and PSD.
-
-    Args:
-      A   : Tensor of shape (..., N, N)
-      tol : numerical tolerance
-
-    Returns:
-      Tensor of shape (...) of bools
-    """
-    # 1) symmetry: |A - A^T| <= tol everywhere
-    diff = torch.abs(A - A.transpose(-1, -2))
-    # all entries must be <= tol
-    symm = diff.le(tol).all(dim=(-2, -1))
-
-    # 2) eigenvalues of symmetric matrix
-    #    use eigvalsh for real-symmetric
-    eigs = torch.linalg.eigvalsh(A)  # shape (..., N)
-    # smallest eigenvalue
-    min_eig = eigs[..., 0]
-
-    # 3) PSD if symmetric AND min_eig >= -tol
-    return symm & (min_eig >= -tol)
---
+++ src/mppi_solver/mppi_solver/src/solver/solver_v2/sampling/standard_normal_noise.py
@@ -0,0 +1,70 @@
+import torch
+import time
+from rclpy.logging import get_logger
+
+
+class StandardSamplling:
+    def __init__(self, n_sample : int, n_horizon : int, n_action : int, device):
+        self.logger = get_logger("Standard_Sampling")
+
+        # Torch GPU
+        self.device = device
+
+        # Sampling Parameter
+        self.n_sample = n_sample
+        self.n_horizon = n_horizon
+        self.n_action = n_action
+
+        # Standard Dev.
+        self.sigma = torch.eye((self.n_action), device = self.device) # 표준편차 0.1
+        self.sigma *= 0.1
+        # self.sigma[:3, :3] *= 3.0
+        # self.sigma[3:, 3:] *= 3.0
+
+
+        self.sigma_matrix = self.sigma.expand(self.n_sample, self.n_horizon, -1, -1)
+
+        self.sigma_update = False
+        self.init_sigma: torch.Tensor = self.sigma.clone()
+
+    def sampling(self):
+        # standard_normal_noise = torch.randn(self.n_sample, self.n_horizon, self.n_action, device=self.device)
+        standard_normal_noise = torch.randn(self.n_sample, 1, self.n_action, device=self.device)
+        standard_normal_noise = standard_normal_noise.expand(-1, self.n_horizon, -1)
+        self.sigma_matrix = self.sigma.expand(self.n_sample, self.n_horizon, -1, -1)
+        noise = torch.matmul(standard_normal_noise.unsqueeze(-2), self.sigma_matrix).squeeze(-2)
+        return noise
+
+
+    def get_sample_joint(self, samples: torch.Tensor, q: torch.Tensor, qdot: torch.Tensor, dt):
+        # samples : (N, T, A)
+        
+        # qdot0 는 (A,) 사이즈인데 얘를 unsqueeze 두번
+        # qdot0 는 (1,1,A)
+        # Expand (N, 1, A)
+        qdot0 = qdot.unsqueeze(0).unsqueeze(0).expand(self.n_sample, 1, self.n_action)  # (n_sample, 1, n_action)
+        q0 = q.unsqueeze(0).unsqueeze(0).expand(self.n_sample, 1, self.n_action)        # (n_sample, 1, n_action)
+        
+        # Sample * dt = dv
+        v = torch.cumsum(samples * dt, dim=1) + qdot0  # (n_sample, n_horizon, n_action)
+        v_prev = torch.cat([qdot0, v[:, :-1, :]], dim=1)  # (n_sample, n_horizon, n_action)
+        dq = v_prev * dt + 0.5 * samples * dt**2
+        # OR / v_prev 가 가속도를 통해 나온건데 굳이 더해줘야하나?
+        # dq = v_prev * dt
+
+        q = torch.cumsum(dq, dim=1) + q0
+
+        return q, v
+
+
+    def get_prev_sample_joint(self, u_prev: torch.Tensor, q: torch.Tensor, qdot: torch.Tensor, dt):
+        qdot0 = qdot.unsqueeze(0).expand(1, self.n_action)  # (1, n_action)
+        q0 = q.unsqueeze(0).expand(1, self.n_action)        # (1, n_action)
+        v = torch.cumsum(u_prev * dt, dim=0) + qdot0  # (n_horizon, n_action)
+
+        v_prev = torch.cat([qdot0, v[:-1, :]], dim=0)  # (n_horizon, n_action)
+
+        dq = v_prev * dt + 0.5 * u_prev * dt**2
+        q = torch.cumsum(dq, dim=0) + q0
+        return q.unsqueeze(0)
+    
